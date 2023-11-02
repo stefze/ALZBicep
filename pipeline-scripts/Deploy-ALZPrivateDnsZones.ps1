a9de9a7b67efb717c42c415193bd132f3a493c3a
Stefze <stefze@microsoft.com>
2023-11-02 22:49:40 +0100

    Redefined Private DNS Zones approach7

--- pipeline-scripts/Deploy-ALZPrivateDnsZones.ps1
+++ pipeline-scripts/Deploy-ALZPrivateDnsZones.ps1
@@ -3,7 +3,7 @@ param (
   [String]$ConnectivitySubscriptionId = "$($env:CONNECTIVITY_SUBSCRIPTION_ID)",
 
   [Parameter()]
-  [String]$ConnectivityResourceGroup = "$($env:CONNECTIVITY_RESOURCE_GROUP)",
+  [String]$PrivateZonesResourceGroup = "$($env:PRIVATE_ZONES_RESOURCE_GROUP)",
 
   [Parameter()]
   [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\custom-dnszones\privatednszones.bicep",
@@ -18,7 +18,7 @@ param (
 # Parameters necessary for deployment
 $inputObject = @{
   DeploymentName        = 'alz-PrivateDNSZones-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
-  ResourceGroupName     = $ConnectivityResourceGroup
+  ResourceGroupName     = $PrivateZonesResourceGroup
   TemplateFile          = $TemplateFile
   TemplateParameterFile = $TemplateParameterFile
   WhatIf                = $WhatIfEnabled
